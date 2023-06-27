3c202d1df28b5c45b4ca57cd356ad173ac248d53
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-27 20:06:38 -0300

    v6

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ValidaToken } from './valida-token';
-import { RefreshToken } from './refresh-token';
-import { BuscaSegredoParameterStore } from "./busca-segredo-parameter-store";
-import { CriaToken } from "./cria-token";
-
-export { ValidaToken, RefreshToken, BuscaSegredoParameterStore, CriaToken }
