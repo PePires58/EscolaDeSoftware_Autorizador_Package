704707be96db56a7d39711057147a4e91350871d
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-26 23:45:05 -0300

    testte v2

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { CriaToken } from "./cria-token";
+import { BuscaSegredoParameterStore } from "./busca-segredo-parameter-store";
+import { RefreshToken } from "./refresh-token";
+import { ValidaToken } from "./valida-token";
+
+module.exports = {
+    "CriaToken": CriaToken,
+    "BuscaSegredoParameterStore": BuscaSegredoParameterStore,
+    "RefreshToken": RefreshToken,
+    "ValidaToken": ValidaToken
+}
