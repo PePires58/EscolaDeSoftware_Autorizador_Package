7433fbcce4eeffce73229febd383cdc586276220
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-26 23:37:58 -0300

    fix test

--- src/tests/validar-token.test.ts
+++ src/tests/validar-token.test.ts
@@ -1,8 +1,8 @@
-import { Usuario } from '././models/usuario';
-import { CriaToken } from '../services/cria-token';
+import { Usuario } from '../models/usuario';
+import { CriaToken } from '../cria-token';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { ValidaToken } from '../services/valida-token';
+import { ValidaToken } from '../valida-token';
 
 
 describe('Testes de validação do token', () => {
