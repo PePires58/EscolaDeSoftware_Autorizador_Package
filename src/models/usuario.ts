58c7e590429d5ea2f17ce647885355151b93d3e1
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-27 19:16:11 -0300

    ajuste

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -1,12 +1,4 @@
-export class Usuario {
-
-    constructor() {
-        this.email = '';
-        this.nome = '';
-        this.sobrenome = '';
-        this.cpf = '';
-    }
-
+export interface Usuario {
     email: string;
     nome: string;
     sobrenome: string;
