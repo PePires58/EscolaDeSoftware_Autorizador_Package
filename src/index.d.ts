7f728b844c8a4f55de7eff01a85439dcfee48ee8
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-27 22:07:07 -0300

    v8

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { BuscaSegredoParameterStore } from "./types/busca-segredo-parameter-store";
+import { CriaToken } from "./types/cria-token";
+import { RefreshToken } from "./types/refresh-token";
+import { ValidaToken } from "./types/valida-token";
+
+export { BuscaSegredoParameterStore, CriaToken, RefreshToken, ValidaToken }
--- src/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { BuscaSegredoParameterStore } from "./busca-segredo-parameter-store";
-import { CriaToken } from "./cria-token";
-import { RefreshToken } from "./refresh-token";
-import { ValidaToken } from "./valida-token";
-
-export { BuscaSegredoParameterStore, CriaToken, RefreshToken, ValidaToken }
